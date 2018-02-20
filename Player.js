a1e153d1c348738637ba70df867fd81e9c6612d8
maze <redsbonefifth@gmail.com>
2018-02-20 16:27:08 +0100

    helpful comments

--- Player.js
+++ Player.js
@@ -2,17 +2,16 @@
 
 class Player {
   constructor(x, y, name, socket){
-    this.x = x;
-    this.y = y;
-    this.name = name;
-    this.socket = socket;
-    this.subscriptions = [];
-    this.chunkUpdates = [];
-    this.changeObj = {};
-    this.hacker = false;
-    this.hackedAt = -1;
-    this.branch = 0;
-    this.online = false;
+    this.x = x; // world pos
+    this.y = y; // world pos
+    this.name = name; // username
+    this.socket = socket; // websocket of the current session
+    this.subscriptions = []; // the chunks which are having updates sent to this client
+    this.chunkUpdates = []; // the chunks which will be sent to this client as a whole
+    this.changeObj = {}; // the terrain updates that the client actually needs
+    this.hacker = false; // whether we are ignoring the player because he is desynched
+    this.hackedAt = -1; // the index of the action which caused desynch
+    this.branch = 0; // the current branch of this session (each desynch spawns new branch)
   }
 
 
