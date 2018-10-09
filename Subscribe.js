335b02b7d7b62933e8305eedf551cfe33364da0c
maze <redsbonefifth@gmail.com>
2018-10-09 14:04:38 +0200

    Rewrote subscribe logic. Works now

--- Subscribe.js
+++ Subscribe.js
@@ -4,12 +4,15 @@ Makes sure that the clients are getting updates for exactly those chunks they ca
 TODO: chunk probes are probably a bad idea and should be substituted by different logic. And the area needs to be bigger. And, oh, may I mention that this doesn't even work? Sometimes I find unloaded chunks.
 */
 
-const CHUNK_PROBES = [
-  {x: +64, y: +96},
-  {x: -64, y: +96},
-  {x: +64, y: -96},
-  {x: -64, y: -96}
-];
+const LOAD_DISTANCE = 1; // chunks of at most this distance away from the player's chunk are loaded.
+const UNLOAD_DISTANCE = 2; // chunks further than this distance away from the player's chunk are unloaded.
+
+// const CHUNK_PROBES = [
+//   {x: +64, y: +96},
+//   {x: -64, y: +96},
+//   {x: +64, y: -96},
+//   {x: -64, y: -96}
+// ];
 
 class Subscribe{
   constructor(map){
@@ -26,13 +29,19 @@ class Subscribe{
   }
 
   unsubscribe(player, chunkx, chunky){
-    console.log('unsub '+player.name+' '+chunkx+' '+chunky);
     for(let i = 0; i < player.subscriptions.length; i++){
       if(player.subscriptions[i].x == chunkx && player.subscriptions[i].y == chunky){
-        player.subscriptions.splice(i, 1);
+        this.unsubscribeIndex(player, i);
         break;
       }
     }
+  }
+
+  unsubscribeIndex(player, index){
+    const chunkx = player.subscriptions[index].x;
+    const chunky = player.subscriptions[index].y;
+    console.log('unsub '+player.name+' '+chunkx+' '+chunky);
+    player.subscriptions.splice(index, 1);
     const chunk = this.map.getChunk(chunkx, chunky);
     for(let i = 0; i < chunk.subscribers.length; i++){
       if(chunk.subscribers[i].name == player.name){
@@ -45,71 +54,54 @@ class Subscribe{
   resubscribe(player, force = false){
     const distance = Math.abs(player.resubscribePosition.x - player.x) + Math.abs(player.resubscribePosition.y - player.y);
     if(distance < 128 && !force){
+      // The player has hardly moved, no point in resubscribing now
       return;
     }
+    // Update position of last resubscribe
     player.resubscribePosition.x = player.x;
     player.resubscribePosition.y = player.y;
 
-    // console.log('resub '+player.name);
-    const newSubscriptions = [];
-    for(let p = 0; p < CHUNK_PROBES.length; p++){
-      const chunkx = Math.floor((player.x + CHUNK_PROBES[p].x)/256);
-      const chunky = Math.floor((player.y + CHUNK_PROBES[p].y)/256);
-      // check if we found a new one
-      let yes = true;
-      for(let i = 0; i < newSubscriptions.length; i++){
-        if(newSubscriptions[i].x == chunkx && newSubscriptions[i].y == chunky){
-          yes = false;
-          break;
-        }
-      }
-      if(yes){
-        newSubscriptions.push({x: chunkx, y: chunky});
-      }
-    }
-    // console.log(newSubscriptions)
+    // Determine which chunk the player is in:
+    const chunkPosX = Math.floor(player.x / 256);
+    const chunkPosY = Math.floor(player.y / 256);
 
-    for(let j = 0; j < player.subscriptions.length; j++){
-      // is this a lost subscription?
-      let yes = true;
-      for(let i = 0; i < newSubscriptions.length; i++){
-        if(
-          newSubscriptions[i].x == player.subscriptions[j].x
-          && newSubscriptions[i].y == player.subscriptions[j].y
-        ){
-          yes = false;
-          break;
-        }
-      }
-      if(yes){
-        // lostSubscriptions.push(newSubscriptions[i]);
-        this.unsubscribe(player, player.subscriptions[j].x, player.subscriptions[j].y);
+    // Unsubscribe from chunks which are too far:
+    for (let c = player.subscriptions.length - 1; c >= 0; c--){
+      const chunkx = player.subscriptions[c].x;
+      const chunky = player.subscriptions[c].y;
+      if(chunkx < chunkPosX-UNLOAD_DISTANCE
+        || chunkx > chunkPosX+UNLOAD_DISTANCE
+        || chunky < chunkPosY-UNLOAD_DISTANCE
+        || chunky > chunkPosY+UNLOAD_DISTANCE){
+        // this chunk is too far away
+        this.unsubscribeIndex(player, c);
       }
     }
 
-    for(let i = 0; i < newSubscriptions.length; i++){
-      // is this a fresh subscription?
-      let yes = true;
-      for(let j = 0; j < player.subscriptions.length; j++){
-        if(
-          newSubscriptions[i].x == player.subscriptions[j].x
-          && newSubscriptions[i].y == player.subscriptions[j].y
-        ){
-          yes = false;
-          break;
+    // Subscribe to chunks which are by the player
+    // (unless already subscribed)
+    for (let i = chunkPosY-LOAD_DISTANCE; i <= chunkPosY+LOAD_DISTANCE; i++){
+      for (let j = chunkPosX-LOAD_DISTANCE; j <= chunkPosX+LOAD_DISTANCE; j++){
+        // check if this subscription exists already
+        let exists = false;
+        for (const s of player.subscriptions){
+          if(s.x === j && s.y === i){
+            exists = true;
+            break;
+          }
         }
-      }
-      if(yes){
-        // freshSubscriptions.push(newSubscriptions[i]);
-        this.subscribe(player, newSubscriptions[i].x, newSubscriptions[i].y);
+        if(exists){
+          continue;
+        }
+        // subscription does not exist. Subscribe:
+        this.subscribe(player, j, i);
       }
     }
-    // console.log(player.subscriptions)
   }
 
   unsubscribeAll(player){
-    for(let i = 0; i < player.subscriptions.length; i++){
-      this.unsubscribe(player, player.subscriptions[i].x, player.subscriptions[i].y);
+    for(let i = player.subscriptions.length-1; i>=0; i--){
+      this.unsubscribeIndex(player, i);
     }
   }
 }
